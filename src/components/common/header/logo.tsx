993fca10532fa9d64561f4bee977b5ad171aabc3
thedonatello <97092727+thedonatello@users.noreply.github.com>
2022-04-08 09:46:04 +0200

    Remove double link on logo component

--- src/components/common/header/logo.tsx
+++ src/components/common/header/logo.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Flex, Text } from 'theme-ui'
-import { MotionFlex } from '../motion-components'
 
 const Logo: React.FC = () => {
   return (
     <Link href='/' passHref>
-      <MotionFlex as='a' sx={{ cursor: 'pointer', position: 'relative', alignItems: 'center', minWidth: 170 }}>
+      <Flex as='a' sx={{ position: 'relative', alignItems: 'center', minWidth: 170 }}>
         <Image src='/assets/images/boundnft_logo.svg' width={36.45} height={29} />
         <Flex ml={15}>
           <Text variant='styles.logo-text'>BOUND</Text>
           <Text sx={{ ml: '1.5px'}} variant='styles.logo-text-bold'>NFT</Text>
         </Flex>
-      </MotionFlex>
+      </Flex>
     </Link>
   )
 }
