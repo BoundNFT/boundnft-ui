95914d3de46a0114ac98ef8669f550a2d9c6adfe
daRalph <daddyRaphael@protonmail.com>
2022-04-13 22:32:12 +0200

    web3modal

---
+++ src/modules/wallet/constants/index.ts
@@ -0,0 +1,31 @@
+import WalletConnectProvider from '@walletconnect/web3-provider';
+
+export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID || 'b49c0bdd7b474ccd9b0ac4ab7ae4820c'
+export const NETWORK = process.env.NEXT_PUBLIC_NETWORK || 'rinkeby'
+
+export const providerOptions = {
+  metamask: {
+    id: 'injected',
+    name: 'MetaMask',
+    type: 'injected',
+    check: 'isMetaMask',
+    package: null
+  },
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: INFURA_ID, // Required
+      network: NETWORK,
+      qrcodeModalOptions: {
+        mobileLinks: [
+          'rainbow',
+          'metamask',
+          'argent',
+          'trust',
+          'imtoken',
+          'pillar'
+        ]
+      }
+    }
+  }
+}
---
+++ src/modules/wallet/hooks/useWallet.ts
@@ -0,0 +1,6 @@
+import React from 'react'
+import { IState, WalletContext } from '../context/wallet'
+
+
+const useWallet = (): IState => React.useContext(WalletContext)
+export default useWallet
