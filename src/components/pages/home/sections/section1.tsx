7780aed9035b4b26e6df9b7cdf826e374305cf41
MichelangeloSan <Michelangelo03012009@protonmail.com>
2022-04-13 10:34:09 +0200

    section1 animations

--- src/components/pages/home/sections/section1.tsx
+++ src/components/pages/home/sections/section1.tsx
@@ -9,13 +9,13 @@ export const Section1 = () => {
   return (
     <Animations>
       <MotionFlex sx={{ width: '100%', maxWidth: 1280, flexDirection: 'column'}}>
-        <MotionFlex sx={{ width: '100%', flexDirection: 'column', fontSize: 'xxxl'}} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <MotionFlex sx={{ width: '100%', flexDirection: 'column', fontSize: 'xxxl'}}>
           <MotionText className='slide-in-title' variant='text.title-white'>{t('label.powering').toUpperCase()}</MotionText>
           <MotionText className='slide-in-title-2'variant='text.title-bold-white'>{t('label.decentralized').toUpperCase()}</MotionText>
           <MotionText className='slide-in-title-3'variant='text.title-bold-green'>{t('label.nft-liquidity').toUpperCase()}</MotionText>
         </MotionFlex>
 
-        <MotionFlex sx={{position: 'relative', flexDirection: 'column', alignItems: 'center', ml: [0, 0, 0], mt: 40}} initial={{ opacity: 0 }} animate={{ opacity: 1}}>
+        <MotionFlex sx={{position: 'relative', flexDirection: 'column', alignItems: 'center', ml: [0, 0, 0], mt: 40}}>
           <MotionBox className='slide-in-logo' sx={{ width: 95, height: 75, position: 'relative'}}>        
             <Image src='/assets/images/boundnft_logo.svg' layout='fill'/>
           </MotionBox>
@@ -25,7 +25,7 @@ export const Section1 = () => {
           </MotionFlex>
         </MotionFlex>
 
-        <MotionFlex className='fade-in-img-right' sx={{ width: '100%',  justifyContent: 'right', ml: [0, 50, 300, 450, 450], mt: [50, 50, -200, -250]}} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <MotionFlex className='fade-in-img-right' sx={{ width: '100%',  justifyContent: 'right', ml: [0, 50, 300, 450, 450], mt: [50, 50, -200, -250]}}>
           <Box sx={{ width: [344, 452, 638, 853], height: [191, 250, 354, 473], position: 'relative'}}>        
             <Image src='/assets/images/top_image_1.png' layout='fill'/>
           </Box>
